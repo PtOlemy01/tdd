c2bafc0ba98290550c30cb6ed095b648cbed9eeb
PtOlemy01 <shapewki@naver.com>
2021-11-22 18:10:20 +0900

    TODO리스트 추가.

--- src/test/java/com/example/tdd/multi/Multiplication.java
+++ src/test/java/com/example/tdd/multi/Multiplication.java
@@ -3,15 +3,27 @@ package com.example.tdd.multi;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/*
+    TODO
+    $5 + 10CHF = $10(환율이 2:1일 경우)
+    $5 * 2 = $10  ( 완료 )
+    amount 를 private로 만들기
+    Dollar 부작용? ( 완료 )
+    Money 반올리?
+    equals()
+    hashCode()
+    */
+
 public class Multiplication {
 
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assertions.assertEquals(10, five.amount);
-        five.times(3);
-        Assertions.assertEquals(15, five.amount);
+        Dollar product = five.times(2);
+
+        Assertions.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assertions.assertEquals(15, product.amount);
     }
 
 }
