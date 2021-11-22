398f07f3705c2ec9803fba7787fe993a72bcc2a4
PtOlemy01 <shapewki@naver.com>
2021-11-22 17:55:07 +0900

    tdd strat

---
+++ src/main/java/com/example/tdd/multi/Dollar.java
@@ -0,0 +1,12 @@
+package com.example.tdd.multi;
+
+public class Dollar {
+    int amount = 10;
+
+    Dollar(int amount){
+
+    }
+    void times(int multiplier){
+
+    }
+}
---
+++ src/test/java/com/example/tdd/multi/Multiplication.java
@@ -0,0 +1,15 @@
+package com.example.tdd.multi;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Multiplication {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        Assertions.assertEquals(10, five.amount);
+    }
+
+}
