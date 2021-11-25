eebc851cc7303ecacf921b68c3dd372087570a66
PtOlemy01 <shapewki@naver.com>
2021-11-25 17:40:41 +0900

    통화작업 끝.

--- src/main/java/com/example/tdd/multi/Money.java
+++ src/main/java/com/example/tdd/multi/Money.java
@@ -25,7 +25,7 @@ public class Money implements Expression{
         return new Money(amount, "CHF");
     }
 
-    Money times(int multiplier){
+    Expression times(int multiplier){
         return new Money(amount * multiplier, currency);
     }
     public boolean equals(Object object){
@@ -37,7 +37,7 @@ public class Money implements Expression{
         return amount + "  " + currency;
     }
 
-    Expression plus(Money addend){
+    public Expression plus(Expression addend){
         return new Sum(this, addend);
     }
 }
